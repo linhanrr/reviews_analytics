b71b1d5bb1d7f5b14d77d04e40a5784726624400
Linhanrr <hank093011@gmail.com>
2021-07-16 19:03:40 +0800

    lookup ver.

--- read.py
+++ read.py
@@ -8,13 +8,48 @@ with open('reviews.txt', 'r') as f:
         if count % 10000 == 0:
             print(len(data))
 
-for d in data:
-    sum_len += len(d)
-print('平均留言長度為', sum_len / len(data), '個字')
 
-new = []
+wc = {}
 for d in data:
-    if len(d) < 100:
-        new.append(d)
-print('一共有', len(new), '筆留言長度小於100')
-print(new[0])
+    words = d.split(' ')
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1  #新增一個key
+for word in wc:
+    if wc[word] > 1000000:
+        print(word, wc[word])
+print(len(wc))
+
+while True:
+    word = input('你想查詢什麼字: ')
+    if word == 'q':
+        break
+    if word in wc:
+        print (word, '出現過的次數為' , wc[word], '次')
+    else:
+        print(word, '沒有出現過喔!')
+
+
+print('感謝使用本查詢功能')
+
+
+
+# for d in data:
+#     sum_len += len(d)
+# print('平均留言長度為', sum_len / len(data), '個字')
+
+# new = []
+# for d in data:
+#     if len(d) < 100:
+#         new.append(d)
+# print('一共有', len(new), '筆留言長度小於100')
+# print(new[0])
+
+# good =[]
+# for g in data:
+#     if 'good' in g:
+#         good.append(g)
+# print('一共有', len(good), '筆留言提到good')
+# print(good[0])
